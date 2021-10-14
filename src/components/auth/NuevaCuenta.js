14c3fabc08556d2c53e354e388960c728b446a6f
Dario Navarrete <rd_navarrete@outlook.com>
2021-10-13 19:04:16 -0500

    Creada estructura basica de la app, compomentes y routing

---
+++ src/components/auth/NuevaCuenta.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const NuevaCuenta = () => {
+    return (
+        <h1>Nueva Cuenta</h1>
+    );
+}
+
+export default NuevaCuenta;
---
+++ src/components/proyectos/Proyectos.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Proyectos = () => {
+    return (
+        <h1>Proyectos nuevos</h1>
+    );
+}
+
+export default Proyectos;
