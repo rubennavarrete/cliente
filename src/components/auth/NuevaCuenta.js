7b332b65263e675a1cd90b77013591d84d9ad0d4
Dario Navarrete <rd_navarrete@outlook.com>
2021-10-25 18:07:17 -0500

    Validando los campos y agregando alertas

--- src/components/auth/NuevaCuenta.js
+++ src/components/auth/NuevaCuenta.js
@@ -1,8 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
+import AlertaContext from '../../context/alertas/alertaContext';
 
 
 const NuevaCuenta = () => {
+
+    // Extraer los valores del context 
+    const alertaContext = useContext(AlertaContext);
+    const { alerta, mostrarAlerta } = alertaContext;
 
     //State para iniciar sesión
     const [usuario, guardarUsuario] = useState({
@@ -28,17 +33,33 @@ const NuevaCuenta = () => {
         e.preventDefault();
 
         //validar que no hay campos vacios
+        if (nombre.trim() === '' ||
+            email.trim() === '' ||
+            password.trim() === '' ||
+            confirmar.trim() === '') {
+            mostrarAlerta('Todos los campos son obligatorios', 'alerta-error');
+            return;
+        }
 
         //Password minimo de 6 caracteres
-        
+        if (password.length < 6) {
+            mostrarAlerta('El password debe ser de almenos 6 caracteres', 'alerta-error');
+            return;
+        }
+
         //Los dos password son iguales
-        
+        if (password !== confirmar) {
+            mostrarAlerta('Los paswords no son iguales!', 'alerta-error');
+            return;
+        }
+
         //Pasar al action
     }
 
 
     return (
         <div className="form-usuario">
+            {alerta ? (<div className={`alerta ${alerta.categoria}`}> {alerta.msg} </div>) : null}
             <div className="contenedor-form sombra-dark">
                 <h1>Obtener una Cuenta</h1>
 
